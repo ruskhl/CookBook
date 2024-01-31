c498a3255a4e8c5cc7f8c54702956004b1819621
ruskhl <ruslana.khlopuk@gmail.com>
2024-01-31 20:45:21 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,29 +1,39 @@
-#import necessary library
-#from tkinter import Button
-from kivy.app import App 
+from kivy.app import App
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.button import Button
+from kivy.uix.textinput import TextInput
+
 import webbrowser
-#from kivy.uix.button import Button
-
-
-def any_Function(instance):
-    webbrowser.open('http://ruslanak.pythonanywhere.com')
 
 
 
-class TutorialApp(App):
+#def any_Function(instance):
+    #webbrowser.open('http://ruslanak.pythonanywhere.com')
+
+
+
+class MainApp(App):
     def build(self):
       self.icon = "icon.png"
+      main_layout = webbrowser.open('http://ruslanak.pythonanywhere.com')
+      main_layout = BoxLayout(orientation = "vertical")
      
 
       #btn1 = Button(text='Open Link' , size=(200,50), size_hint=(None, None))
       #btn1.bind(on_press=any_Function)
 
       #Bind function with button
+      #return btn1
 
-      return webbrowser.open('http://ruslanak.pythonanywhere.com')
+      
+         
+      #return webbrowser.open('http://ruslanak.pythonanywhere.com')
+      return main_layout
       
 
 
 if __name__ == '__main__':
-    TutorialApp().run()
+    app =MainApp()
+    MainApp().run()
+
 
